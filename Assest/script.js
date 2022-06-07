14c95b154067948a79d3638fea9d14978569a5bc
SahilMamedov <sahilim@code.edu.az>
2022-06-08 03:35:27 +0400

    feat: location click

---
+++ Assest/script.js
@@ -0,0 +1,17 @@
+let select=document.querySelector(".header-location")
+let active = document.getElementById("select-location");
+let close =document.getElementById("close")
+select.addEventListener("click",function(){
+
+    active.style.display="flex"
+    active.style.justifyContent="center"
+    
+});
+
+close.addEventListener("click",function(){
+    active.style.display="none";
+})
+
+
+
+
