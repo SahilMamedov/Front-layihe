57c51079557298386e3500ee434a96beb1e1d9d6
SahilMamedov <sahilim@code.edu.az>
2022-06-09 19:38:39 +0400

    feat: register js

---
+++ Assest/register/register.js
@@ -0,0 +1,26 @@
+let select=document.querySelector(".header-location")
+let active = document.getElementById("select-location");
+let close =document.getElementById("close")
+let categories=document.getElementById("categories")
+let sidebar=document.querySelector(".menu-sidebar")
+
+categories.addEventListener("click",function(ev){
+    ev.preventDefault()
+    if(sidebar.classList.contains("hidden")){
+        sidebar.classList.remove("hidden")
+    }
+    else{
+        sidebar.classList.add("hidden")
+    }
+    
+})
+select.addEventListener("click",function(){
+
+    active.style.display="flex"
+    active.style.justifyContent="center"
+});
+
+close.addEventListener("click",function(){
+    active.style.display="none";
+})
+
