df1d6ab492eec25e80fbb7bcae01ebc1af951cd4
SahilMamedov <sahilim@code.edu.az>
2022-06-09 15:30:41 +0400

    feat: swipper slider

--- Assest/script.js
+++ Assest/script.js
@@ -27,3 +27,22 @@ close.addEventListener("click",function(){
 
 
 
+const swiper = new Swiper('.swiper', {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+      },
+      loop: true,
+      grabCursor: true,
+    //   autoplay: {
+    //     delay: 2000,
+    //     stopOnLastSlide: false,
+    //     disableOnInteraction: false,
+    //   },
+      speed: 1000,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
